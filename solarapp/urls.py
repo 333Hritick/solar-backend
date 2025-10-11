63fa4d6c7f883293b16f115eccb552a1061e432c
333Hritick <hritickkumar3138@gmail.com>
2025-10-11 13:29:47 +0530

    Fix root URL to return JSON response instead of index.html

--- solarapp/urls.py
+++ solarapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from.views import create_quote
+from .views import home
 
 
 urlpatterns = [
     path('quote/',create_quote,name='create_quote'),
+     path('', home),  
 ]
