5154b6f592c6df718f3d57490f321d53c953be82
333Hritick <hritickkumar3138@gmail.com>
2025-10-11 13:45:26 +0530

    Fix import path for home view

--- solarapp/urls.py
+++ solarapp/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from.views import create_quote
-from .views import home
 
 
 urlpatterns = [
     path('quote/',create_quote,name='create_quote'),
-     path('', home),  
 ]
