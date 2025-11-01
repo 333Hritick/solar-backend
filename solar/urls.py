2e9380d61f9fbf02ab1e5f6b19470b584ed11543
333Hritick <hritickkumar3138@gmail.com>
2025-11-01 23:07:04 +0530

    Updated  Django backend with new form features and fixes

--- solar/urls.py
+++ solar/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('solarapp.urls')),
     path('', home),  
+
    
 
 ]
--- solarapp/urls.py
+++ solarapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from.views import create_quote
+from .import views
 
 
 urlpatterns = [
     path('quote/',create_quote,name='create_quote'),
+    path('calculate_emi/', views.calculate_emi, name='calculate_emi'),
+    
 ]
