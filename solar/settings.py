9d680a942b2973c203b040bf0ebf975abfdd33dc
hritick3138 <hritickkumar3138@gmail.com>
2026-09-11 14:15:20 +0530

    static file

--- solar/settings.py
+++ solar/settings.py
@@ -165,3 +165,6 @@ AUTHENTICATION_BACKENDS = [
 # Celery (Redis broker)
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
